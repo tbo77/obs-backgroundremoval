a6696603675e5b992ca1ae1553422aa89b4abcc6
tbo77 <blackone@gmx.de>
2022-01-17 16:50:27 +0100

    Update install-dependencies-ubuntu.sh

--- ci/linux/install-dependencies-ubuntu.sh
+++ ci/linux/install-dependencies-ubuntu.sh
@@ -17,3 +17,8 @@ sudo apt-get install -y \
 sudo wget -O /usr/include/obs/obs-frontend-api.h https://raw.githubusercontent.com/obsproject/obs-studio/25.0.0/UI/obs-frontend-api/obs-frontend-api.h
 
 sudo ldconfig
+
+# Just a test...
+sudo apt install -y libobs-dev libopencv-dev language-pack-en wget git build-essential cmake
+sudo wget https://github.com/microsoft/onnxruntime/releases/download/v1.7.0/onnxruntime-linux-x64-1.7.0.tgz
+sudo tar xzvf onnxruntime-linux-x64-1.7.0.tgz --strip-components=1 -C /usr/local/ --wildcards "*/include/*" "*/lib*/"

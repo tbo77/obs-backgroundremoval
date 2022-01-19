7cc9120130cce581c07d53492637434fbdad0d6e
tbo77 <blackone@gmx.de>
2022-01-19 17:19:44 +0100

    Update package-ubuntu.sh

--- ci/linux/package-ubuntu.sh
+++ ci/linux/package-ubuntu.sh
@@ -6,7 +6,7 @@ script_dir=$(dirname "$0")
 source "$script_dir/../ci_includes.generated.sh"
 
 export GIT_HASH=$(git rev-parse --short HEAD)
-export PKG_VERSION="1-$GIT_HASH-$BRANCH_SHORT_NAME-git"
+export PKG_VERSION="$SEMVER"
 
 if [[ "$BRANCH_FULL_NAME" =~ "^refs/tags/" ]]; then
 	export PKG_VERSION="$BRANCH_SHORT_NAME"

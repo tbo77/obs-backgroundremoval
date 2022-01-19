0b46c2afda18fddb8373f837262df33bfa122c23
tbo77 <blackone@gmx.de>
2022-01-19 17:12:45 +0100

    Update build-ubuntu.sh

--- ci/linux/build-ubuntu.sh
+++ ci/linux/build-ubuntu.sh
@@ -2,5 +2,5 @@
 set -ex
 
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib ..
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib -DARG_LOG=1 ..
 make -j4

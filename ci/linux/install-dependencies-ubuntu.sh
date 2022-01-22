d0446b0a7526981ba4224fa775c4f9c5116b8de9
tbo77 <blackone@Tuxedo-20.04>
2022-01-22 11:51:02 +0100

    Added onnx runtime to ubuntu package

--- ci/linux/install-dependencies-ubuntu.sh
+++ ci/linux/install-dependencies-ubuntu.sh
@@ -21,3 +21,5 @@ sudo ldconfig
 
 sudo wget https://github.com/microsoft/onnxruntime/releases/download/v1.7.0/onnxruntime-linux-x64-1.7.0.tgz
 sudo tar xzvf onnxruntime-linux-x64-1.7.0.tgz --strip-components=1 -C /usr/local/ --wildcards "*/include/*" "*/lib*/"
+mkdir libonnxruntime
+tar xzvf onnxruntime-linux-x64-1.7.0.tgz --strip-components=2 -C libonnxruntime/ --wildcards "*/lib*/"
